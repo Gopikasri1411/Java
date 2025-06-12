e9dc110b1dfb2112b7223d57791075551bd10633
GOPIKA SRI <96835179+Gopikasri1411@users.noreply.github.com>
2025-06-12 14:45:01 +0530

    Create N * N matrix.java

---
+++ Pattern printing/N * N matrix.java
@@ -0,0 +1,8 @@
+static void printnn(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
