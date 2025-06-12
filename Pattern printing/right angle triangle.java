b436e26d92d1ee4f408d624e4c93353ea8dae53b
GOPIKA SRI <96835179+Gopikasri1411@users.noreply.github.com>
2025-06-12 14:46:03 +0530

    Create right angle triangle.java

---
+++ Pattern printing/right angle triangle.java
@@ -0,0 +1,8 @@
+static void righttriange(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
