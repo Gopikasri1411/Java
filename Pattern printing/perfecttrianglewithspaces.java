18dbe0aeafb990e1b9f5c15cfc379fd2f0349406
GOPIKA SRI <96835179+Gopikasri1411@users.noreply.github.com>
2025-06-12 14:56:13 +0530

    Create perfecttrianglewithspaces.java

---
+++ Pattern printing/perfecttrianglewithspaces.java
@@ -0,0 +1,12 @@
+static void perfect(int n) {
+        for(int i=1;i<=n;i++){
+            int totalspaces=n-i;
+            for(int s=0;s<totalspaces;s++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
