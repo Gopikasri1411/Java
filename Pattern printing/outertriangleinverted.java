b48c42c7e4954c1a89b8c7c0a3e4205ce6730dd7
GOPIKA SRI <96835179+Gopikasri1411@users.noreply.github.com>
2025-06-12 15:01:57 +0530

    Create outertriangleinverted.java

---
+++ Pattern printing/outertriangleinverted.java
@@ -0,0 +1,18 @@
+static void outertriangle(int n) {
+        for(int i=n;i>=1;i--){
+            int outerspace=n-i;
+            for(int s=0;s<outerspace;s++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                if(j==1 || j==i||i==n) {
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
