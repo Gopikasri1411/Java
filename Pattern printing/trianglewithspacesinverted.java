f3e5f0a59e77624b62d58065b1e253f79e610f1a
GOPIKA SRI <96835179+Gopikasri1411@users.noreply.github.com>
2025-06-12 14:54:01 +0530

    Create trianglewithspacesinverted.java

---
+++ Pattern printing/trianglewithspacesinverted.java
@@ -0,0 +1,13 @@
+static void trianglewithspaces(int n) {
+        for(int i=5;i>=1;i--){
+            int totalspaces=n-i;
+            for (int s = 0; s < totalspaces; s++) {
+                System.out.print(" ");
+
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
