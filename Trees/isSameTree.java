78cc35b7eda6a6e755cfbfb22e056e4f8dd7dbf6
GOPIKA SRI <96835179+Gopikasri1411@users.noreply.github.com>
2025-06-26 21:57:07 +0530

    Update isSameTree.java

--- Trees/isSameTree.java
+++ Trees/isSameTree.java
@@ -1,9 +1,12 @@
 public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p==null && q==null){
-            return true;
+        if(p==null || q==null){           
+            return !(p==null ^ q==null); // instead of this line ,you can also do the below method for a base condition.
         }
-        if(p==null || q==null){
-            return false;
-        }
+        // if(p==null && q==null){
+        //     return true;
+        // }
+        // if(p==null || q==null){
+        //     return false;
+        // }
         return p.val==q.val && isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
     }
