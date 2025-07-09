76cac65a10c5a31960f86e9660206144c0a7468e
GOPIKA SRI <96835179+Gopikasri1411@users.noreply.github.com>
2025-07-09 12:17:14 +0530

    Update MergeTrees.java

--- Trees/MergeTrees.java
+++ Trees/MergeTrees.java
@@ -8,9 +8,7 @@ public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
             }
             return root1;
         }
-        if(root1!=null && root2!=null){
-            root1.val=root1.val+root2.val;
-        }
+        root1.val=root1.val+root2.val;
         root1.left=mergeTrees(root1.left,root2.left);
         root1.right=mergeTrees(root1.right,root2.right);
         return root1;
