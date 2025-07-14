a77d950745bdd3aeca1ae360a3c9b21d88393995
GOPIKA SRI <96835179+Gopikasri1411@users.noreply.github.com>
2025-07-14 22:52:04 +0530

    Update buildTree.java

--- Trees/buildTree.java
+++ Trees/buildTree.java
@@ -16,3 +16,27 @@ public TreeNode buildTree(int[] preorder, int[] inorder) {
         return node;
 
     }
+
+//optimized code
+public TreeNode buildTree(int[] preorder, int[] inorder) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<inorder.length;i++){
+            map.put(inorder[i],i);
+        }
+        return helper(preorder,inorder,0,inorder.length-1,map);
+    }
+    int index=0;
+    public TreeNode helper(int[] preorder,int[] inorder,int l,int r,HashMap<Integer,Integer> map){
+        if(l>r){
+            return null;
+        }
+        int current=map.get(preorder[index]);
+        index++;
+        TreeNode node=new TreeNode(inorder[current]);
+        if(l==r){
+            return node;
+        }
+        node.left=helper(preorder,inorder,l,current-1,map);
+        node.right=helper(preorder,inorder,current+1,r,map);
+        return node;
+    }
