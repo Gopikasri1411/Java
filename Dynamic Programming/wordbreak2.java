f6552d2349e88db631eed0f9573e8a18398a6439
GOPIKA SRI <96835179+Gopikasri1411@users.noreply.github.com>
2025-07-31 20:30:34 +0530

    Update wordbreak2.java

--- Dynamic Programming/wordbreak2.java
+++ Dynamic Programming/wordbreak2.java
@@ -11,3 +11,22 @@ public int wordBreak(String s, List<String> wordDict) {
         }
         return count;
     }
+
+//optimized one: memoization
+HashMap<String,Integer> map=new HashMap<>();
+    public int wordBreak(String s, List<String> wordDict) {
+        if(s==""){
+            return 1;
+        }
+        if(map.containsKey(s)){
+            return map.get(s);
+        }
+        int count=0;
+        for(int i=0;i<wordDict.size();i++){
+            if(s.length()>=wordDict.get(i).length() && wordDict.get(i).equals(s.substring(0,wordDict.get(i).length()))){
+                count+=wordBreak(s.substring(wordDict.get(i).length(),s.length()),wordDict);
+            }
+        }
+        map.put(s,count);
+        return count;
+    }
