bbbbf2824806459ced7cc5563e26a7781731ba35
GOPIKA SRI <96835179+Gopikasri1411@users.noreply.github.com>
2025-08-01 14:25:53 +0530

    Create BiPartite.java

---
+++ Graph/BiPartite.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean isBipartite(int V, int[][] edges) {
+    int[] colors = new int[V];
+    Arrays.fill(colors, -1);
+
+    // Build adjacency list
+    List<List<Integer>> adj = new ArrayList<>();
+    for (int i = 0; i < V; i++) adj.add(new ArrayList<>());
+    for (int[] edge : edges) {
+        int u = edge[0], v = edge[1];
+        adj.get(u).add(v);
+        adj.get(v).add(u); // undirected
+    }
+
+    for (int i = 0; i < V; i++) {
+        if (colors[i] == -1) {
+            if (!check(i, adj, colors)) return false;
+        }
+    }
+    return true;
+}
+
+boolean check(int start, List<List<Integer>> adj, int[] color) {
+    Queue<Integer> queue = new LinkedList<>();
+    queue.add(start);
+    color[start] = 0;
+
+    while (!queue.isEmpty()) {
+        int node = queue.poll();
+        for (int neighbor : adj.get(node)) {
+            if (color[neighbor] == -1) {
+                color[neighbor] = 1 - color[node];
+                queue.add(neighbor);
+            } else if (color[neighbor] == color[node]) {
+                return false; // Conflict
+            }
+        }
+    }
+    return true;
+}
+
+}
