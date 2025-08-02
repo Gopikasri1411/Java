7278877d29c2eb83aa0a8d3b24ac9ec63537d9be
GOPIKA SRI <96835179+Gopikasri1411@users.noreply.github.com>
2025-08-02 13:47:35 +0530

    Update BiPartite.java

--- Graph/BiPartite.java
+++ Graph/BiPartite.java
@@ -1,3 +1,4 @@
+//bfs
 class Solution {
     public boolean isBipartite(int V, int[][] edges) {
     int[] colors = new int[V];
@@ -37,6 +38,49 @@ boolean check(int start, List<List<Integer>> adj, int[] color) {
         }
     }
     return true;
+}
+
+}
+
+
+//dfs
+class Solution {
+    public boolean isBipartite(int V, int[][] edges) {
+    int[] colors = new int[V];
+    Arrays.fill(colors, -1);
+
+    // Build adjacency list
+    List<List<Integer>> adj = new ArrayList<>();
+    for (int i = 0; i < V; i++) adj.add(new ArrayList<>());
+    for (int[] edge : edges) {
+        int u = edge[0], v = edge[1];
+        adj.get(u).add(v);
+        adj.get(v).add(u); // undirected
+    }
+
+    for (int i = 0; i < V; i++) {
+        if (colors[i] == -1) {
+            if (!check(i, 0,adj, colors)) return false;
+        }
+    }
+    return true;
+}
+
+boolean check(int start, int colorprev,List<List<Integer>> adj, int[] color) {
+    color[start]=1-colorprev;
+    for(int i:adj.get(start)){
+        if(color[i]==-1){
+                if(!check(i,color[start],adj,color)){
+                    return false;
+                }
+            }else{
+                if(color[i]==color[start]){
+                    return false;
+                }
+        }
+    }
+    return true;
+    
 }
 
 }
