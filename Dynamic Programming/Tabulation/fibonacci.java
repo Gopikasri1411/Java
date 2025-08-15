7852e7ff6bdb3c1fa9a36434b1ee3a8a79003ef9
GOPIKA SRI <96835179+Gopikasri1411@users.noreply.github.com>
2025-08-15 19:03:42 +0530

    Create fibonacci.java

---
+++ Dynamic Programming/Tabulation/fibonacci.java
@@ -0,0 +1,13 @@
+void func(int n){
+  int[] arr=new int[n+1];
+  arr[1]=1;
+  for(int i=0;i<=n;i++){
+    if(i+1<=n){
+      arr[i+1]+=arr[i];
+    }
+    if(i+2<=n){
+      arr[i+2]+=arr[i];
+    }
+  }
+return arr[n];
+}
