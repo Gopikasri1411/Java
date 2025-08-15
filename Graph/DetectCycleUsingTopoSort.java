0f8ceec7271f66280b50f5b8e5a2424edc606d0b
GOPIKA SRI <96835179+Gopikasri1411@users.noreply.github.com>
2025-08-15 17:38:00 +0530

    Create DetectCycleUsingTopoSort.java

---
+++ Graph/DetectCycleUsingTopoSort.java
@@ -0,0 +1,36 @@
+public boolean isCyclic(int V, int[][] edges) {
+        // code here
+        int[] indegree=new int[V];
+        List<List<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<V;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<edges.length;i++){
+            adj.get(edges[i][0]).add(edges[i][1]);
+        }
+        
+        for(int i=0;i<edges.length;i++){
+            indegree[edges[i][1]]++;
+        }
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0){
+                q.add(i);
+            }
+        }
+        ArrayList<Integer> topo=new ArrayList<>();
+        while(!q.isEmpty()){
+            int node=q.peek();
+            q.remove();
+            topo.add(node);
+            for(int it:adj.get(node)){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.add(it);
+                }
+            }
+        }
+        return topo.size()!=V;
+        
+    }
+    
